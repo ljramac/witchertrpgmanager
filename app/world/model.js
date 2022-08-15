ca71d367995cfc5656b5639fe9b6a64974458817
Lino Rama <linojrc@ext.inditex.com>
2022-08-16 00:37:47 +0200

    wip: seed world

---
+++ app/world/model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const base = require("../base/model");
+
+const Schema = mongoose.Schema;
+
+const RegionSchema = new Schema({
+  ...base.schema,
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  meta: {
+    type: Object,
+    default: {}
+  }
+});
+
+const KingdomSchema = new Schema({
+  ...base.schema,
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  region: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Region",
+    autopopulate: true
+  },
+  meta: {
+    type: Object,
+    default: {}
+  }
+});
+
+module.exports = {
+  Kingdom: mongoose.model("Kingdom", KingdomSchema, "Kingdoms"),
+  Region: mongoose.model("Region", RegionSchema, "Regions"),
+};
